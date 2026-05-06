92c50668f07b9e83b2829923dbad26879489c174
javiermonrio <javier.monrio@alumnos.upm.es>
2026-05-06 16:24:28 +0200

    CLASES PIEZA
    Creación de clases de cada tipo de pieza,

---
+++ ARCHON_WARHAMMER 40K/src/ClaseArcher.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Pieza.h"
+#include "Arena.h"
+#include "Color.h"
+#include <string>
+
+class ClaseArcher : public Pieza {
+public:
+    ClaseArcher(Bando b, sf::Vector2i pos, std::string tipo);
+
+
+    bool poderMover(sf::Vector2i destino, const std::vector<Pieza*>& otrasPiezas, bool esDestinoOcupado) override;
+    void procesarMovimientoArena(sf::Vector2f direccion, float dt, Arena& arena);
+    void dibujar(sf::RenderWindow& window, Estado estadoActual);
+};
